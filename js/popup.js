31547c20db9a7518bd6e8e4343df64645b4b2ca0
Liubov-d <nessy_moek@mail.ru>
2020-02-29 22:06:33 +0300

    Проверка на переполнение

--- js/popup.js
+++ js/popup.js
@@ -1,37 +1,18 @@
 var link = document.querySelector(".button-wright-us");
 var popup = document.querySelector(".modal-wright-us");
-var close = popup.querySelector(".modal-close");
+var close = document.querySelector(".modal-close");
 var form = popup.querySelector("form");
+var validation = popup.querySelectorAll("validation");
+
 var name = popup.querySelector("[name=name]");
 var email = popup.querySelector("[name=email]");
 var letter = popup.querySelector("[name=letter]");
 
- var isStorageSupport = true;
-  var storage = "";
-  
-  try {
-    storage = localStorage.getItem("name");
-    storage = localStorage.getItem("email");
-  } catch (err) {
-    isStorageSupport = false;
-  }
-
-
   link.addEventListener("click", function (evt) {
     evt.preventDefault();
     popup.classList.add("modal-show");
-    if (storage) {
-      name.value = storage;
-      email.focus();
-    } else {
-      name.focus();
-    }
-     if (storage) {
-      name.value = storage;
-      email.value = storage;
-      letter.focus();
-    }
-    });
+    name.focus();
+  });
 
     close.addEventListener("click", function (evt) {
     evt.preventDefault();
@@ -40,17 +21,14 @@ var letter = popup.querySelector("[name=letter]");
   });
 
     form.addEventListener("submit", function (evt) {
-      if (!name.value || !email.value) {
+      for (var i = 0; i < validation.length; i++) {
+      if (!validation[i].value) {
     evt.preventDefault();
     popup.classList.remove("modal-error");
     popup.offsetWidth = popup.offsetWidth;
-    popup.classList.add("modal-error");
-    } else {
-      if (isStorageSupport) {
-        localStorage.setItem("name", name.value);
-        localStorage.setItem("email", email.value);
-      }
-    }
+    popup.classList.add("modal-error")
+  }
+  }
   });
 
     window.addEventListener("keydown", function (evt) {
